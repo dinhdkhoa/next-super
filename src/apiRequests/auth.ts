1639e528e9002d314b9ba0068868475469436201
dinhdkhoa <kddinh212@gmail.com>
2024-06-29 23:38:23 +0700

    feat: Logout Route Handler

--- src/apiRequests/auth.ts
+++ src/apiRequests/auth.ts
@@ -5,6 +5,19 @@ const authAPI = {
     loginServer: (body: LoginBodyType) => http.post<LoginResType>('auth/login', body),
     loginClient: (body: LoginBodyType) => http.post<LoginResType>('api/auth/login', body, {
         baseUrl: ''
+    }),
+    logoutServer: (body: {
+        refreshToken: string,
+        accessToken: string
+    }) => http.post<{ message: string }>('auth/logout', {
+        refreshToken: body.refreshToken
+    }, {
+        headers: {
+            'Authorization': `Bearer ${body.accessToken}`
+        }
+    }),
+    logoutClient: () => http.post<{message: string}>('api/auth/logout', null, {
+        baseUrl: ''
     })
 }
 
---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,41 @@
+import authAPI from "@/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from 'jsonwebtoken'
+import { HttpError } from "@/lib/https";
+
+interface AppRequest<T = any> extends Request {
+    json: () => Promise<T>;
+}
+
+
+export async function POST() {
+    const nextCookies = cookies()
+    const accessToken = nextCookies.get('accessToken')?.value
+    const refreshToken = nextCookies.get('refreshToken')?.value
+    if (!accessToken || !refreshToken){
+        return Response.json({
+            message: 'Không tìm thấy access token hoặc refresh token'
+        }, {
+            status: 200
+        })
+    }
+
+    try {
+        const logoutRes = await authAPI.logoutServer({
+            accessToken,
+            refreshToken
+        })
+        return Response.json({
+            message: 'Logout thành công'
+        }, {
+            status: 200
+        })
+    } catch (error) {
+        return Response.json({
+            message: 'Logout gọi đến server lỗi'
+        }, {
+            status: 200
+        })
+    }
+}

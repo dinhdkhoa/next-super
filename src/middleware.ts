0f1f0c0b075a6a005f36c69283d84e6fe8862705
dinhdkhoa <kevin_mccain1718@yahoo.com>
2024-11-04 17:27:07 +0700

    feat: middleware employee

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,6 +12,7 @@ export function middleware(request: NextRequest) {
     
     if(isPublicPath) return NextResponse.next()
 
+        console.log('isPublicPath', pathname)
     const accessToken = request.cookies.get('accessToken')
     const refreshToken = request.cookies.get('refreshToken')
 
@@ -48,6 +49,7 @@ export function middleware(request: NextRequest) {
 
     if(role == 'Guest' && !isGuestPath) return NextResponse.redirect(new URL('/', request.url))
     if(role !== 'Guest' && isGuestPath) return NextResponse.redirect(new URL('/manage/dashboard', request.url))
+    if(role == 'Employee' && !isEmployeePath) return NextResponse.redirect(new URL('/manage/dashboard', request.url))
 
     return NextResponse.next()
 }

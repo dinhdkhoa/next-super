b0ac07b0c82ba9d0547c826d564bca81a7b8f6a3
dinhdkhoa <kevin_mccain1718@yahoo.com>
2024-11-20 22:12:54 +0700

    feat: Migrate React.Context To Zustand In AppProvider

--- src/components/app-provider.tsx
+++ src/components/app-provider.tsx
@@ -1,69 +1,48 @@
 'use client'
+import useAuthStore, { useAuthStoreBase } from "@/hooks/zustand/useAuthStore"
+import useStorePersist from "@/hooks/zustand/useStorePersist"
+import { socket } from "@/lib/socket"
 import StorageService from "@/lib/storage"
+import { TokenPayload } from "@/types/jwt.types"
 import {
   QueryClient,
   QueryClientProvider
 } from "@tanstack/react-query"
-import { createContext, ReactNode, useCallback, useContext, useEffect, useState } from 'react'
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { RoleType, TokenPayload } from "@/types/jwt.types"
 import jwt from 'jsonwebtoken'
-import { socket } from "@/lib/socket"
+import { ReactNode, useEffect } from 'react'
 
+export default function AppProvider({ children }: { children: ReactNode }) {
+  const setRole = useAuthStore.use.setRole()
 
-const AppContext = createContext<{
-  setRole: (role?: RoleType) => void,
-  isAuth: boolean,
-  role?: RoleType
-}>({
-  isAuth: false,
-  role: undefined ,
-  setRole: (role?: RoleType) => {}
-})
-
-export const useAppContext = () => useContext(AppContext)
-
-export default function AppProvider({children} : {children: ReactNode}) {
-  const [role, setRoleState] = useState<RoleType | undefined>(undefined)
-
-useEffect(() => {
-  const accessToken = StorageService.getAccessToken()
-  if(accessToken){
-    const jwtInfo = jwt.decode(accessToken) as TokenPayload
-    setRoleState(jwtInfo.role)
-    if(!socket.isConnected){
-      socket.connect()
+  useEffect(() => {
+    const accessToken = StorageService.getAccessToken()
+    if(accessToken){
+      const jwtInfo = jwt.decode(accessToken || '') as TokenPayload
+      setRole(jwtInfo?.role)
+      if (!socket.isConnected) {
+        socket.connect()
+      }
     }
-  } 
-  return () => {
-    socket.disconnect();
-  };
-},[])
+    return () => {
+      socket.disconnect();
+    }
+  }, [])
 
-const setRole = useCallback((role?: RoleType) => {
-  if(role) {
-    setRoleState(role)
-  } else {  
-    StorageService.removeTokens()
-  }
-}, [])
 
-const isAuth = Boolean(role)
-
-const queryClient = new QueryClient({
+  const queryClient = new QueryClient({
     defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-        }
+      queries: {
+        refetchOnWindowFocus: false,
+      }
     }
-})
+  })
 
   return (
-   <AppContext.Provider value={{isAuth,setRole,role}}>
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-    </AppContext.Provider>
   )
 }
+

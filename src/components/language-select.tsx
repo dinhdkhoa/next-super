e8a269b54e08af43dcdb9b9747fdc0d28caaca9b
dinhdkhoa <kevin_mccain1718@yahoo.com>
2024-12-21 08:47:46 +0700

    feat: Update LanguageSelect With i18n-with-routing

--- src/components/language-select.tsx
+++ src/components/language-select.tsx
@@ -10,20 +10,32 @@ import {
 import { Locale, locales } from '@/i18n/i18n'
 import { useLocale, useTranslations } from 'next-intl';
 import { getUserLocale, setUserLocale } from '@/i18n/locale';
+import { usePathname, useRouter } from '@/i18n/routing';
+import { useParams } from 'next/navigation';
 
 function LanguageSelect() {
     const t = useTranslations('LocaleSwitcher');
     const locale = useLocale()
+    const pathname = usePathname();
+    const router = useRouter();
+    const params = useParams();
+
     return (
-        <Select value={locale} onValueChange={(value: Locale) => setUserLocale(value)}>
+        <Select value={locale} onValueChange={(value: Locale) => router.replace(
+            // @ts-expect-error -- TypeScript will validate that only known `params`
+            // are used in combination with a given `pathname`. Since the two will
+            // always match for the current route, we can skip runtime checks.
+            { pathname, params },
+            { locale: value }
+        )}>
             <SelectTrigger className="w-[60px] text-xs">
                 <SelectValue placeholder={t('label')} />
             </SelectTrigger>
             <SelectContent >
-                {locales.map(lan => 
-                <SelectItem value={lan} key={lan} className="text-xs">{lan.toUpperCase()}</SelectItem>
+                {locales.map(lan =>
+                    <SelectItem value={lan} key={lan} className="text-xs">{lan.toUpperCase()}</SelectItem>
                 )}
-                
+
             </SelectContent>
         </Select>
     )
